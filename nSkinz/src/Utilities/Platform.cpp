ae0b959c8a64e77bc84fda858af023ba0e8d5d2a
namazso <xynmzs@gmail.com>
2017-04-25 18:20:02 +0200

    welp

--- nSkinz/src/Utilities/Platform.cpp
+++ nSkinz/src/Utilities/Platform.cpp
@@ -47,8 +47,25 @@ uintptr_t platform::FindPattern(const char* szModule, const char* szPattern, con
 
 bool platform::IsCodePtr(void* p)
 {
-	MEMORY_BASIC_INFORMATION out;
-	VirtualQuery(p, &out, sizeof(out));
+	/*constexpr auto flags = (PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
 
-	return (out.AllocationBase && out.Type && out.Protect & PAGE_EXECUTE);
+	MEMORY_BASIC_INFORMATION mbi{};
+	if (VirtualQuery(p, &mbi, sizeof(mbi)))
+		return (mbi.Protect & flags) && !(mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS));
+
+	return false;*/
+
+	/*__try
+	{
+		auto x = *reinterpret_cast<uint8_t*>(p);
+		return true;
+	}
+	__except (_exception_code() == EXCEPTION_ACCESS_VIOLATION ?
+		EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH)
+	{
+		return false;
+	}*/
+
+	// Ok fuck this randomly crashing shit
+	return !IsBadCodePtr(reinterpret_cast<FARPROC>(p));
 }
